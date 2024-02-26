c07da74d199746579e0cd24980ef2c5848ff5d7b
phucxp12vn <vphuc8230tth@gmail.com>
2024-02-26 11:28:35 +0700

    add docker file and config

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,31 @@
+import dns from 'dns';
 import path from 'path';
 
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+dns.setDefaultResultOrder('verbatim');
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react()],
+  plugins: [
+    react({
+      include: '**/*.tsx',
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
   server: {
+    watch: {
+      usePolling: true
+    },
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://build-card-server:3000',
         changeOrigin: true,
       },
     },
